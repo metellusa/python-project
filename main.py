52bd173dc5054f625ce4e0f2fa757a06f107edb3
Anaja <metellusa@gmail.com>
2020-08-03 14:11:44 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+# def printFunction(name):
+#     price = 25
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi {name}. You are awesome! This meal will cost you: $');
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     printFunction('Anaja')
+
+name = input("Please enter your name: ")
+favoriteColor = input("What is your favorite color? ")
+print(f'Your name is {name} and you like the color {favoriteColor}')
