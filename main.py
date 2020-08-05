7504fdf0c806979e34d63bf57d6c8fe3e4cb6b8f
Anaja <metellusa@gmail.com>
2020-08-05 15:07:15 -0400

    Changed the code

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-# This is a sample Python script.
+numbers = [3, 3, 3, 3, 8, 8]
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-# def printFunction(name):
-#     price = 25
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi {name}. You are awesome! This meal will cost you: $');
-#
-# # Press the green button in the gutter to run the script.
-# if __name__ == '__main__':
-#     printFunction('Anaja')
-
-name = input("Please enter your name: ")
-favoriteColor = input("What is your favorite color? ")
-print(f'Your name is {name} and you like the color {favoriteColor}')
+for item in numbers:
+    print("x" * item)
